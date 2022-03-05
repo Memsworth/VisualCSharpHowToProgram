b0d4b30b325bbc605cf5b66df2c674bbe9538b1e
Abubakar Ahmed <abubakarahmed4497@gmail.com>
2022-03-05 16:15:44 +0530

    Update InventoryLevelCalculator.cs

--- ChapterFive/InventoryLevelCalculator.cs
+++ ChapterFive/InventoryLevelCalculator.cs
@@ -2,5 +2,29 @@ namespace VisualCSharpHowToProgram.ChapterFive;
 
 public class InventoryLevelCalculator
 {
+    private int _itemCode;
+    private int _openingUnits;
+    private int _unitsPurchased;
+    private int _unitsIssued;
     
+    public string UnitName { get; private set; }
+
+    public int ItemCode
+    {
+        get => _itemCode;
+        set
+        {
+            if(value > 0)
+            {
+                _itemCode = value;
+            }
+        }
+    }
+
+    public int OpeningUnits
+    {
+        get => 
+    }
+
+
 }
